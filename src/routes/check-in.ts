45b0bec330046e17a95121bb05ea4320f34d910e
Willame Mouzinho <willame.dev@gmail.com>
2024-04-07 10:42:42 -0300

    feat: add check in route

---
+++ src/routes/check-in.ts
@@ -0,0 +1,42 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '../lib/prisma'
+
+export async function checkIn(server: FastifyInstance) {
+	server.withTypeProvider<ZodTypeProvider>().get(
+		'/attendees/:attendeeId/check-in',
+		{
+			schema: {
+				params: z.object({
+					attendeeId: z.coerce.number().int(),
+				}),
+				response: {
+					201: z.null,
+				},
+			},
+		},
+		async (request, reply) => {
+			const { attendeeId } = request.params
+
+			const attendeeCheckIn = await prisma.checkIn.findUnique({
+				where: {
+					attendeeId,
+				},
+			})
+
+			if (attendeeCheckIn !== null) {
+				throw new Error('Attendee has already checked in to this event!')
+			}
+
+			await prisma.checkIn.create({
+				data: {
+					attendeeId,
+				},
+			})
+
+			return reply.code(201).send()
+		},
+	)
+}

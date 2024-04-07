1535f6da6cd8ef9975a75bee97e48dba3a1bd4ed
Willame Mouzinho <willame.dev@gmail.com>
2024-04-07 11:22:08 -0300

    feat: add get event attendees route

---
+++ prisma/migrations/20240407141834_add_cascades/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_attendees" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "event_id" TEXT NOT NULL,
+    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_attendees" ("created_at", "email", "event_id", "id", "name") SELECT "created_at", "email", "event_id", "id", "name" FROM "attendees";
+DROP TABLE "attendees";
+ALTER TABLE "new_attendees" RENAME TO "attendees";
+CREATE UNIQUE INDEX "attendees_email_key" ON "attendees"("email");
+CREATE UNIQUE INDEX "attendees_email_event_id_key" ON "attendees"("email", "event_id");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
---
+++ src/routes/get-event-attendees.ts
@@ -0,0 +1,105 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '../lib/prisma'
+
+export async function getEventAttendees(server: FastifyInstance) {
+	server.withTypeProvider<ZodTypeProvider>().get(
+		'/events/:eventId/attendees',
+		{
+			schema: {
+				params: z.object({
+					eventId: z.string().uuid(),
+				}),
+				querystring: z.object({
+					query: z.string().nullish(),
+					pageIndex: z.string().nullish().default('0').transform(Number),
+				}),
+				response: {
+					200: z.object({
+						attendees: z.array(
+							z.object({
+								id: z.number(),
+								name: z.string(),
+								email: z.string().email(),
+								createdAt: z.date(),
+								checkedInAt: z.date().nullable(),
+							}),
+						),
+						totalAttendees: z.number(),
+					}),
+				},
+			},
+		},
+		async (request, reply) => {
+			const { eventId } = request.params
+			const { query, pageIndex } = request.query
+
+			const [event, attendees, totalAttendees] = await Promise.all([
+				prisma.event.findUnique({
+					where: {
+						id: eventId,
+					},
+				}),
+				prisma.attendee.findMany({
+					where: query
+						? {
+								eventId,
+								name: {
+									contains: query,
+								},
+							}
+						: {
+								eventId,
+							},
+					select: {
+						id: true,
+						name: true,
+						email: true,
+						createdAt: true,
+						checkIn: {
+							select: {
+								createdAt: true,
+							},
+						},
+					},
+					take: 10,
+					skip: 10 * pageIndex,
+					orderBy: {
+						createdAt: 'desc',
+					},
+				}),
+				prisma.attendee.count({
+					where: query
+						? {
+								eventId,
+								name: {
+									contains: query,
+								},
+							}
+						: {
+								eventId,
+							},
+				}),
+			])
+
+			if (event === null) {
+				throw new Error('Event not found.')
+			}
+
+			return reply.code(200).send({
+				attendees: attendees.map((attendee) => {
+					return {
+						id: attendee.id,
+						name: attendee.name,
+						email: attendee.email,
+						createdAt: attendee.createdAt,
+						checkedInAt: attendee.checkIn?.createdAt ?? null,
+					}
+				}),
+				totalAttendees,
+			})
+		},
+	)
+}
